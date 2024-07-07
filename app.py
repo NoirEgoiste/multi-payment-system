801128e99bff36df8a66d7dbd55d58119c12091c
Noir_Egoiste <dmitreykyrchin@gmail.com>
2024-07-08 02:28:44 +0300

    add docker

--- app.py
+++ app.py
@@ -42,7 +42,3 @@ async def handle_webhook(request: Request):
     redirect = response_data.get("invoice_url")
 
     return RedirectResponse(url=redirect, status_code=302)
-
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
